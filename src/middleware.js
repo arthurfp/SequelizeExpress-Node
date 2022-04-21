3f63b56315f54a6f3e2732de3a3509aba7235a70
Arthur Ferreira <fepi.arthur@gmail.com>
2022-04-20 22:02:16 -0300

    Added middleware

---
+++ src/middleware.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const app = express()
+
+function middleware(req, res, next) {
+    const { name } = req.query
+    if (!name) {
+        res.status(403).json({ message: 'Missing required parameter' })
+    } else {
+        next()
+    }
+}
+
+function log_middleware(req, res, next) {
+    console.log('req params =>', req.method, req.query, req.params)
+    next()
+}
+
+app.use(log_middleware)
+
+app.use('/api', middleware)
+
+// Load a static middleware
+app.use(express.static('static', {
+    extensions: ['html', 'htm', 'js', 'css']
+}))
+
+// route
+app.get('/test', (req, res) => {
+    res.json({
+        message: 'test'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is runing')
+})
